7db32e80f47773ac890441d6d748dac933bc9967
Kim Böndergaard <threezool@gmail.com>
2020-03-08 17:58:33 +0100

    Setting up the database context and seeding the data when created

---
+++ Threezool.dev/Data/DbContext/InMemoryContext.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Threezool.dev.Data.Seed;
+using Threezool.dev.Models;
+
+namespace Threezool.dev.Data.DbContext
+{
+    public class InMemoryContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+
+        public InMemoryContext(DbContextOptions<InMemoryContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Project>().HasData(ProjectSeed.GetProjectSeedData());
+            
+            modelBuilder.Entity<Skill>().HasData(ProjectSeed.GetProjectSeedData());
+        }
+    }
+}
