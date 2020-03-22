77375f9febd25ac4ba0ad4a53ee075f6400bd8e2
Kim Böndergaard <threezool@gmail.com>
2020-03-22 12:37:52 +0100

    Added a contact service

--- Threezool.dev/Services/ContactService.cs
+++ Threezool.dev/Services/ContactService.cs
@@ -1,7 +1,15 @@
+using Threezool.dev.Data.Models;
+using Threezool.dev.Data.Repository;
+
 namespace Threezool.dev.Services
 {
     public class ContactService : IContactService
     {
-        
+        private readonly IRepository<Contact> contactService;
+
+        public ContactService(IRepository<Contact> contactService)
+        {
+            this.contactService = contactService;
+        }
     }
 }
